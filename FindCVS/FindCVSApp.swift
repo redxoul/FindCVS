aa740d39b504bbf8b917b7642ddb6fca9d01a376
SwiftyCody <soldolly@gmail.com>
2023-02-11 06:42:04 +0900

    FindCVS first commit

---
+++ FindCVS/FindCVSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindCVSApp.swift
+//  FindCVS
+//
+//  Created by solgoon on 2023/02/08.
+//
+
+import SwiftUI
+
+@main
+struct FindCVSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
